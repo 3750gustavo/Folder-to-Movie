48a8c2b14208e8170f06acc042443490aa369379
3750gustavo <40867494+3750gustavo@users.noreply.github.com>
2025-03-10 13:43:57 -0300

    feat: Enhance package installation feedback with error handling and success messages

--- package_manager.py
+++ package_manager.py
@@ -4,7 +4,14 @@ import sys
 import importlib
 
 def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    process = subprocess.Popen([sys.executable, "-m", "pip", "install", package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    if process.returncode != 0:
+        print(f"An error occurred while installing {package}.\n")
+        print(f"Output: {output.decode('utf-8')}")
+        print(f"Error: {error.decode('utf-8')}\n")
+    else:
+        print(f"{package} installed successfully using: \nPython Path: {sys.executable}.\n")
 
 def ensure_package(module, package=None):
     if package is None:
@@ -12,5 +19,5 @@ def ensure_package(module, package=None):
     try:
         importlib.import_module(module)
     except ImportError:
-        print(f"{module} not found. Installing...")
+        print(f"{module} not found. Installing...\n")
         install(package)
